0b6eac34fa8a74b97b1efbe473c5ede5cf997171
jaga-live <jagadheesh6@gmail.com>
2022-10-18 19:08:09 +0530

    feat - improved Hindi detection

--- src/data/greetings.js
+++ src/data/greetings.js
@@ -1,10 +1,4 @@
-const greetings = [
-    "hello",
-    "hey",
-    "hi",
-    "hola",
-    "whats up"
-]
+const greetings = ["hello", "hey", "hi", "hola", "whats up", "wassup", "dood", "dewd"];
 
 module.exports = {
     greetings
